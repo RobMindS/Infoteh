930f34acf41a5b29ce0d523ea20f9ad0b1af6805
Robert <robert_m@polbox.tv>
2022-07-19 18:21:42 +0300

    Initial Commit

---
+++ Infoteh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infoteh
+//
+//  Created by admin on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
